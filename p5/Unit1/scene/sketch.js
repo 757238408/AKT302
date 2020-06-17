37a9461f4217f6f4027ba98c583b2350e28a024a
757238408 <67019652+757238408@users.noreply.github.com>
2020-06-17 23:39:26 +0800

    saving for the first time.

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,31 @@
 function setup() {
   createCanvas(500, 500);
-  
 }
 
 function draw() {
-  background(100) ; 
-  // put your shapes here!
-  
-  
-  text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
+  background(152,245,255);
+  noStroke();
 
-  
 
-function mouseReleased() { 
-  // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
 
- 
+  fill(72,61,139)
+  arc(400, 250, 150, 150,PI + QUARTER_PI , PI - QUARTER_PI, PIE);
+
+  fill(106,90,205);
+  ellipse(275, 250, 100,100);
+
+  fill(123,105,238)
+  ellipse(187.5, 250, 75,75);
+
+  fill(132,112,255)
+  ellipse(125, 250, 50,50);
+
+  fill(250,245,205)
+  rect(25, 325, 450,150 , 10);
+
+  fill(0);
+  ellipse(380, 200, 10,10);
+
+  print("Greedy beans");
+
+}
