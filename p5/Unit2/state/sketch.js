f3f80a5557ab3975a7938cad15501a34b9a816be
757238408 <67019652+757238408@users.noreply.github.com>
2020-06-22 11:43:58 +0800

    6/22 class

---
+++ p5/Unit2/state/sketch.js
@@ -0,0 +1,34 @@
+let state = 0;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  switch (state) {
+    case 0:
+    background('white');
+    text("we are in 0",100,100);
+    break;
+
+    case 1:
+    background('red');
+    text("we are in 1",100,100);
+    break;
+
+    case 2:
+    background('yellow');
+    text("we are in 2",100,100);
+    break;
+  }
+
+function mouseReleased(){
+  state = state + 1;
+if(state > 2){
+  state = 0;
+}
+
+}
+
+}
