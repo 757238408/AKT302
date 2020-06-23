7e9a4eac68363f808ead8703ee3b34fe0be48937
757238408 <67019652+757238408@users.noreply.github.com>
2020-06-24 04:52:17 +0800

    6/24

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -1,103 +1,92 @@
-var myState = 0;
-var myTimer = 0;
-var font;
-var x = 0;
-var pic1;
+var pic1,pic2,pic3,pic4,f;
+let state = 0 ;
 let mic;
-let y = 337;
-var vol = 0;
-var pic2;
-var song1;
-
-function preload() {
-  font = loadfont('assets/HoboStd.otf');
-  pic1 = loadImage('assets/a.jpg');
-  pic2 = loadImage('assets/b.jpg');
-  song1 = loadSound('assets/d.mp3')
-}
+let vol;
+let timer = 0 ;
+let x = 500 ;
 
 function setup() {
-  createCanvas(700, 400);
+  createCanvas(600, 600);
+  f = loadFont("assets/HoboStd.otf");
+  pic1 = loadImage("assets/1-01.png");
+  pic2 = loadImage("assets/2-02.png");
+  pic3 = loadImage("assets/3-03.png");
+  pic4 = loadImage("assets/boat.png");
   mic = new p5.AudioIn();
-  textFont(font);
   mic.start();
+  //textmode(CENTER);
 }
 
 function draw() {
-  image(pic1, 0, 0);
-switch (myState) {
+  textFont(f);
+  vol = mic.getLevel();
+  switch (state) {
 
 case 0:
-  background("#ffcb08");
-  image(pic1, 0, 0);
-  fill("white");
+  background(127,255,170);
   textSize(20);
-  fill(random(255), random(255), random(255));
-  textSize(30);
-  text("zedd", x, 200);
-      x += 3;
-      if (x > 700) {
-        x = 0;
-      }
+  text("Dragon Boat Festival",215,80);
+  textSize(12);
+  timer++;
+  text(timer,30,590);
+if(timer > 100){
+  text("The festival commemorates Qu Yuan,a minister in the servive of the Chu Emperor.",50,200);
+  }
+if(timer > 200){
+  text("Townspeople jumped into their boats and tried in vain to save him.",50,250);
+  }
+if(timer > 300){
+  text("Despairing over corruption at court,Qu threw himself into a river.",50,300);
+  }
+if(timer > 400){
+  text("Then,hoping to distract hungry fish from his body,the people scattered rice on the water.",50,350);
+  }
+
+if(timer > 600){
+state = state + 1;
+    }
+
 break;
 
 case 1:
-  background(100);
-  image(pic1, 0, 0);
-  myTimer++;
-if (myTimer >= 10) {
-  myTimer = 4;
-  myState = 2;
-}
-  break;
-
-  case 2:
-  background(100);
-  image(pic1, 0, 0);
-  vol = mic.getLevel();
-  if (vol >= 0.2) {
-  myState = 3;
-  song1.play();
-}
-  image(pic2, 312, 270);
-  textSize(40);
-  text("best dj", 150, 200);
-break;
-
-case 3:
-  background(200);
-  image(pic2, 312, y);
-  y = y - 10;
-if (y <= 0) {
-  myState = 4;
-  song1.stop();
+  clear();
+  background(127,255,170);
+  textSize(10);
+  text(x,30,590);
+  textSize(20);
+  text("Dragon boat racing",215,80);
+  image(pic4,x,100,400,400);
+  x = x - 1;
+if(x < - 350){
+  state =  state + 1;
 }
 break;
 
-case 4:
-  textSize(79);
-  text("great voice", 244, 171);
-  y = 337
-  background("#ffcb08");
-break;
+case 2:
+  clear();
+  background(127,255,170);
+  textSize(15);
+  text(vol.toFixed(3), 555,590);
+  text("blowing to open the dumpling ",200,500);
+if (vol > .001) {
+  image(pic2,300,100,300,300);
+  image(pic3,10,100,300,300);
+  textSize(50);
+  text("Rice Dumpling",155,80);
   }
+else{
+    image(pic1,150,100,300,300);
+    textSize(20);
+    text("Rice Dumpling",225,80);
+  }
+  break;
+}
+
 }
 
 function mouseReleased() {
-  if (myState == 0) {
-    myState = 1;
-  }
-  if (myState == 2) {
-  myState = 3;
-  }
-
-
-  if (myState == 4) {
-    myState = 0;
-  }
-
-  print.log(mouseX + "," + mouseY);
-}
-function touchStarted() {
-  getAudioContext().resume();
+  state++;state==2
+if (state > 2)
+state = 0;
+timer = 0;
 }
